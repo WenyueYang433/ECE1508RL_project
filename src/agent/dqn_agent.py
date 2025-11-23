8518edeebef21713d8f8d50b1240e55722748f79
Lingyun Guo <lingyun.guo@mail.utoronto.ca>
2025-11-22 20:18:09 -0500

    add extra layer

--- src/agent/dqn_agent.py
+++ src/agent/dqn_agent.py
@@ -8,15 +8,16 @@ from utils.replay_buffer import ReplayBuffer
 class DQN(nn.Module):
     def __init__(self, num_actions, feature_size):
         super(DQN, self).__init__()
-        # increased hidden layer sizes for more model capacity
         self.fc1 = nn.Linear(feature_size, 128)
         self.fc2 = nn.Linear(128, 64)
-        self.out = nn.Linear(64, num_actions)
+        self.fc3 = nn.Linear(64, 32)
+        self.out = nn.Linear(32, num_actions)
         self.act = nn.ReLU()
 
     def forward(self, x):
         x = self.act(self.fc1(x))
         x = self.act(self.fc2(x))
+        x = self.act(self.fc3(x))
         q = self.out(x)
         return q
 
